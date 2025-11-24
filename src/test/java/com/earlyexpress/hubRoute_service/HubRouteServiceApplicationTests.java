7f58fe783350ac8e07e391ede36092198ee651c3
vsuminv <tnalsdl5075@naver.com>
2025-11-24 17:32:59 +0900

    feat : 프로젝트 셋팅

---
+++ src/test/java/com/earlyexpress/hubRoute_service/HubRouteServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.earlyexpress.hubRoute_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HubRouteServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
